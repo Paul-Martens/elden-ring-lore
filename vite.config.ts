64a767d05e76d7eb55097c78b9de7c9231b09203
Paul <github@goonie.nl>
2024-10-15 23:41:39 +0200

    Added sass-embedded package to fix Vite's current Sass implementation

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
     port: 8000,
   },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
